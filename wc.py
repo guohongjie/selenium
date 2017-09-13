d14b00c8241702591284a46bbc560e6e981a74e6
guohongjie <31028523+guohongjie@users.noreply.github.com>
2017-09-13 17:21:16 +0800

    Update wc.py

--- wc.py
+++ wc.py
@@ -6,14 +6,14 @@ from bs4 import BeautifulSoup
 class wc(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
-        self.url ="http:////192.168.98.22:8013"
+        self.url =""
     def error(self):
          return 'This is Error'
     def test_wc(self):
         try:
             self.driver.get(self.url)
-            self.driver.find_element_by_xpath("//input[@name='userId']").send_keys('yumi')
-            driver.find_element_by_xpath("//input[@name='password']").send_keys('111111')
+            self.driver.find_element_by_xpath("//input[@name='userId']").send_keys('')
+            driver.find_element_by_xpath("//input[@name='password']").send_keys('')
             self.driver.page_source
         except Exception as e:
             print 'wc'
